86b8aebb1f5c3735bf1ad34ba5c40cdd58d95b78
Zumin Li <zumin.li.work@gmail.com>
2026-06-07 14:36:29 +0800

    fix: restore provideZoneChangeDetection and provideBrowserGlobalErrorListeners

--- projects/admin-portal/src/app/app.config.ts
+++ projects/admin-portal/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, APP_INITIALIZER } from '@angular/core';
+import { ApplicationConfig, APP_INITIALIZER, provideZoneChangeDetection, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -13,9 +13,11 @@ function initAuth(authService: AuthService) {
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withInterceptors([apiInterceptor])),
     provideAnimationsAsync(),
+    provideBrowserGlobalErrorListeners(),
     { provide: ENVIRONMENT, useValue: environment },
     { provide: APP_INITIALIZER, useFactory: initAuth, deps: [AuthService], multi: true },
   ],
